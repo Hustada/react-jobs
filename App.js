b34ca4dbddfebd0551b1fb8b8550240eec854355
Mark Hustad <hustada80@gmail.com>
2018-07-16 10:00:02 -0500

    create stack navigator and import settings and review screens

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 
+//import screens
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = createBottomTabNavigator({
       welcome: WelcomeScreen,
-      auth: AuthScreen
+      auth: AuthScreen,
+      main: {
+        screen: createBottomTabNavigator({
+          map: MapScreen,
+          deck: DeckScreen,
+          review: {
+            screen: createStackNavigator({
+              review: ReviewScreen,
+              settings: SettingsScreen
+            })
+          }
+        })
+      }
     });
 
     return (

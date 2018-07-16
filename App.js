b53e23db6e3dcad72e27a7582fbecb4bcbed99e2
Mark Hustad <hustada80@gmail.com>
2018-07-16 09:46:32 -0500

    render authscreen welcomescreen with routes

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation';
 
 import AuthScreen from './screens/AuthScreen';
-import WelcomeScreen from './screens/WelcomeScreen'
+import WelcomeScreen from './screens/WelcomeScreen';
 
 export default class App extends React.Component {
   render() {
-
+    const MainNavigator = createBottomTabNavigator({
+      welcome: WelcomeScreen,
+      auth: AuthScreen
+    });
 
     return (
       <View style={styles.container}>
-        
+        <MainNavigator />
       </View>
     );
   }
@@ -21,7 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });

07a87e5dac0d0f4780c371401ab27955df88a9c4
Mark Hustad <hustada80@gmail.com>
2018-07-18 10:33:01 -0500

    add button to last slide

--- components/Slides.js
+++ components/Slides.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements';
 
 //get dimensions of device.
-const SCREEN_WIDTH = Dimensions.get('window').width
-
+const SCREEN_WIDTH = Dimensions.get('window').width;
+//find last slide and add button
 class Slides extends Component {
+	renderLastSlide(index) {
+		if(index === this.props.data.length -1) {
+			return (
+				<Button 
+					title="Onwards!"
+					raised
+				/>
+			);
+		}
+	}
 	//for every object create new view that contains slide text
 	renderSlides() {
-		return this.props.data.map((slide) => {
+		return this.props.data.map((slide, index) => {
 			return(
 				<View key={slide.text} 
 					style={[styles.slideStyle, {backgroundColor: slide.color }]}
 				>
 					<Text style={styles.textStyle}>{slide.text}</Text>
+					{this.renderLastSlide(index)}
 				</View>
 			);
 		});
